6e70d2c8e1e1d7a082cd4d8bc29123c0aea58add
Mwapsam <mwapsam@gmail.com>
2022-08-14 14:56:33 +0800

    Add firebase config file

---
+++ firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "long-random-key",
+    authDomain: "default-domain-of-your-app",
+    projectId: "name-project -id",
+    storageBucket: "storage-bucket",
+    messagingSenderId: "messageSenderId",
+    appId: "random-key",
+    measurementId: "random-key"
+};
+
+export default firebaseConfig;
